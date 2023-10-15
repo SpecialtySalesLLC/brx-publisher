827b24e3d6ab109a91662c1e677f89b60271c7ee
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-10-15 17:45:39 -0400

    Publish a sample message to device/dev-iot-device-1/data

--- publisher.py
+++ publisher.py
@@ -1,58 +1,119 @@
-#!/usr/bin/python
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
-
-# use this program to test the AWS IoT certificates received by the author
-# to participate to the spectrogram sharing initiative on AWS cloud
-
-# this program will publish test mqtt messages using the AWS IoT hub
-# to test this program you have to run first its companion awsiotsub.py
-# that will subscribe and show all the messages sent by this program
-
-import paho.mqtt.client as paho
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import sys
+import threading
+import time
+import json
 import os
-import socket
-import ssl
-from time import sleep
-from random import uniform
 
-connflag = False
+received_all_event = threading.Event()
 
-def on_connect(client, userdata, flags, rc):
-    global connflag
-    connflag = True
-    print("Connection returned result: " + str(rc) )
+target_ep = 'a1brrzwqiohmvf-ats.iot.us-east-2.amazonaws.com'
+thing_name = 'dev-iot-device-1'
+cert_filepath = './DeviceCertificate.pem'
+private_key_filepath = './private.pem'
+ca_filepath = './AmazonRootCA1.pem'
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+pub_topic = 'device/{}/data'.format(thing_name)
+sub_topic = 'app/data'
 
-#def on_log(client, userdata, level, buf):
-#    print(msg.topic+" "+str(msg.payload))
+# Callback when connection is accidentally lost.
 
-mqttc = paho.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-#mqttc.on_log = on_log
 
-awshost = "a1brrzwqiohmvf-ats.iot.us-east-2.amazonaws.com"
-awsport = 8883
-clientId = "dev-iot-device-1"
-thingName = "dev-iot-device-1"
-caPath = "AmazonRootCA1.pem.crt"
-certPath = "DeviceCertificate.pem.crt"
-keyPath = "private.pem.key"
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
 
-mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 
-mqttc.connect(awshost, awsport, keepalive=60)
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(
+        return_code, session_present))
 
-mqttc.loop_start()
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
 
-while 1==1:
-    sleep(0.5)
-    if connflag == True:
-        tempreading = uniform(20.0,25.0)
-        mqttc.publish("temperature", tempreading, qos=1)
-        print("msg sent: temperature " + "%.2f" % tempreading )
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+# Callback when the subscribed topic receives a message
+
+
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("Received message from topic '{}': {}".format(topic, payload))
+
+
+# Spin up resources
+event_loop_group = io.EventLoopGroup(1)
+host_resolver = io.DefaultHostResolver(event_loop_group)
+client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+proxy_options = None
+
+mqtt_connection = mqtt_connection_builder.mtls_from_path(
+    endpoint=target_ep,
+    port=8883,
+    cert_filepath=cert_filepath,
+    pri_key_filepath=private_key_filepath,
+    client_bootstrap=client_bootstrap,
+    ca_filepath=ca_filepath,
+    on_connection_interrupted=on_connection_interrupted,
+    on_connection_resumed=on_connection_resumed,
+    client_id=thing_name,
+    clean_session=True,
+    keep_alive_secs=30,
+    http_proxy_options=proxy_options)
+
+print("Connecting to {} with client ID '{}'...".format(
+    target_ep, thing_name))
+
+# Connect to the gateway
+while True:
+    try:
+        connect_future = mqtt_connection.connect()
+# Future.result() waits until a result is available
+        connect_future.result()
+    except:
+        print("Connection to IoT Core failed...  retrying in 5s.")
+        time.sleep(5)
+        continue
     else:
-        print("waiting for connection...")
+        print("Connected!")
+        break
+
+# Subscribe
+print("Subscribing to topic " + sub_topic)
+subscribe_future, packet_id = mqtt_connection.subscribe(
+    topic=sub_topic,
+    qos=mqtt.QoS.AT_LEAST_ONCE,
+    callback=on_message_received)
+
+subscribe_result = subscribe_future.result()
+print("Subscribed with {}".format(str(subscribe_result['qos'])))
+
+while True:
+    print('Publishing message on topic {}'.format(pub_topic))
+
+    hello_world_message = {
+        'message': 'Hello from {} in the AWS IoT Workshop'.format(thing_name)
+    }
+
+    message_json = json.dumps(hello_world_message)
+    mqtt_connection.publish(
+        topic=pub_topic,
+        payload=message_json,
+        qos=mqtt.QoS.AT_LEAST_ONCE)
+    time.sleep(5)
