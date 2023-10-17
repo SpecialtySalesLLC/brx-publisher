6cc7c634177d799ca0f4c5fa00a8d71ec1842de4
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-10-17 14:01:43 -0400

    Refactor message structure

--- publisher.py
+++ publisher.py
@@ -19,12 +19,12 @@ private_key_filepath = './private.pem'
 ca_filepath = './AmazonRootCA1.pem'
 pub_topic = 'device/{}/data'.format(thing_name)
 
+type_list = ['valves', 'sensors']
 location_list = ['system', 'lane1', 'lane2', 'lane2', 'lane3', 'lane4']
 valves_name_list = ['main_water', 'main_drain', 'chem1',
                     'chem2', 'bath', 'fill', 'drain', 'flash', 'air_blow', 'pod']
 sensors_name_list = ['water_meter', 'temperature',
                      'chem1_level', 'chem2_level', 'pod_level']
-
 
 # Callback when connection is accidentally lost.
 
@@ -63,53 +63,36 @@ def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     print("Received message from topic '{}': {}".format(topic, payload))
 
 
-def subscribe_topic(topic):
-    print("Subscribing to topic " + topic)
+def subscribe_topic():
+    print("Subscribing to topic " + pub_topic)
     subscribe_future, packet_id = mqtt_connection.subscribe(
-        topic=topic,
+        topic=pub_topic,
         qos=mqtt.QoS.AT_LEAST_ONCE,
         callback=on_message_received)
     subscribe_result = subscribe_future.result()
     print("Subscribed with {}".format(str(subscribe_result['qos'])))
 
 
-def publish_topic(topic, message):
-    full_pub_topic = '{}/{}'.format(pub_topic, topic)
-    print('Publishing message on topic {}'.format(full_pub_topic))
+def get_random_message():
+    message_type = choice(type_list)
+    message = {
+        'device_name': thing_name,
+        'device_id': '1234567890',
+        'message_type': message_type,
+        'location': choice(location_list),
+        'name': choice(valves_name_list) if type == 'valves' else choice(sensors_name_list),
+        'state': randint(0, 1) if type == 'valves' else uniform(0.00, 1000.00),
+        'time_reading': time.time()
+    }
+    return message
+
+
+def publish_random_messages():
+    message = get_random_message()
+    print('Publishing message on topic {}'.format(pub_topic))
     message_json = json.dumps(message)
     mqtt_connection.publish(
-        topic=full_pub_topic, payload=message_json, qos=mqtt.QoS.AT_LEAST_ONCE)
-
-
-def get_valves_message():
-    valvesMessage = {
-        'device_name': thing_name,
-        'device_id': '1234567890',
-        'location': choice(location_list),
-        'name': choice(valves_name_list),
-        'state': randint(0, 1),
-        'time_reading': time.time()
-    }
-    return valvesMessage
-
-
-def get_sensors__message():
-    sensorsMessage = {
-        'device_name': thing_name,
-        'device_id': '1234567890',
-        'location': choice(location_list),
-        'name': choice(sensors_name_list),
-        'state': uniform(0.0, 1000.00),
-        'time_reading': time.time()
-    }
-    return sensorsMessage
-
-
-def publish_messages(frequency):
-    while True:
-        publish_topic('valves', get_valves_message())
-        publish_topic('sensors', get_sensors__message())
-        time.sleep(frequency)
+        topic=pub_topic, payload=message_json, qos=mqtt.QoS.AT_LEAST_ONCE)
 
 
 # Spin up resources
@@ -140,7 +123,7 @@ print("Connecting to {} with client ID '{}'...".format(
 while True:
     try:
         connect_future = mqtt_connection.connect()
-# Future.result() waits until a result is available
+        # Future.result() waits until a result is available
         connect_future.result()
     except:
         print("Connection to IoT Core failed...  retrying in 5s.")
@@ -151,7 +134,6 @@ while True:
         break
 
 # Subscribe then publish
-subscribe_topic('valves')
-subscribe_topic('sensors')
-
-publish_messages(10)
+while True:
+    publish_random_messages()
+    time.sleep(10)
